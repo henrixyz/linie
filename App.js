c1dc085224c57dd287dbe7b40a8f534ec95aadb1
henrique <115552341+henrixyz@users.noreply.github.com>
2024-05-08 14:13:42 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react'
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity} from 'react-native';
+// import { NavigationContainer } from '@react-navigation/native';
+
+// import { Teste } from './src/Text'
 
 export default function App() {
+  const [nome,setNome] = useState('');
+  const [email,setEmail] = useState('');
+  const [senha,setSenha] = useState('');
+  
+  const cadastro = () => {
+  alert(nome);
+  alert(email);
+  alert(senha);
+  //aqui fazer chamada no cadastro
+  }
+
+  // function cadastro(){
+  //   alert('ok');
+  // }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar hidden />
+      <Image style={{width:300,height:200}}source={require('./assets/img/linie_default.png')}/>
+      <TextInput placeholder="nome:" style={styles.textInput} onChangeText={text=>setNome(text)}/>
+      <TextInput placeholder="email:" style={styles.textInput} onChangeText={text=>setEmail(text)}/>
+      <TextInput secureTextEntry={true} placeholder="senha:" style={styles.textInput} onChangeText={text=>setSenha(text)}/>
+
+      <TouchableOpacity style={styles.btmCadastro} onPress={()=>cadastro()}>
+        <Text style={{color:'white', textAlign:'center'}}>Cadastrar</Text>
+      </TouchableOpacity>
+      <StatusBar style="dark"/>
     </View>
+    // Teste()
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding:20
   },
+  textInput:{
+    width:'100%',
+    height:40,
+    backgroundColor:'white',
+    borderRadius:20,
+    paddingLeft:10,
+    borderWidth:3,
+    borderColor:'#FF66CC',
+    marginBottom:10
+  },
+  btmCadastro:{
+    width:'50%',
+    height:48,
+    backgroundColor:'#d29fe0',
+    borderRadius:20,
+    marginBottom:10,
+    justifyContent:'center'
+  }
 });
